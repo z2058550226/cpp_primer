4407ae3bd2ecc9e30da5f22dcdbc3bdfb56dd6bf
suika <2058550226@qq.com>
2020-06-19 10:39:38 +0800

    ch2

---
+++ ch1/Sales_item2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Sales_item.h>
+
+using namespace std;
+int main(){
+    Sales_item item1,item2;
+    cin >> item1 >> item2;
+    if (item1.isbn() == item2.isbn())
+    {
+        cout << item1+item2 << endl;
+        return 0;
+    }
+
+    cerr << "Data must refer to same ISBN" << endl;
+    
+    return -1;
+}
---
+++ ch2/test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned u = 10;
+    unsigned u2 = 42;
+    int i = -42;
+    cout << i + i << endl;
+    cout << i + u << endl;
+    cout << u - u2 << endl;
+
+    cout << "a rellasdjfksjdlfjalsdjfljsdljf  skdjfl "
+    "sdfjo sowoow "<< endl;
+    string sk = u8"这是suika";
+    cout << sk.size() << endl;
+    return 0;
+}
